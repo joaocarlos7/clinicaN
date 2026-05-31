07d7b6ce3340c28aec123b339f8decc6d964b2dd
João Carlos <joaocarlos.machado05@gmail.com>
2026-05-31 20:03:56 -0300

    feat: consultationMapping
    fix: consultationService

--- src/main/java/dev/clinican/mapping/ConsultationMapping.java
+++ src/main/java/dev/clinican/mapping/ConsultationMapping.java
@@ -1,4 +1,59 @@
 package dev.clinican.mapping;
 
+import dev.clinican.dto.ConsultationDto;
+import dev.clinican.entity.Consultation;
+import dev.clinican.entity.Doctor;
+import dev.clinican.entity.Patient;
+import dev.clinican.entity.TbUser;
+import dev.clinican.repository.DoctorRepository;
+import dev.clinican.repository.PatientRepository;
+import dev.clinican.repository.TbUserRepository;
+
 public class ConsultationMapping {
+
+    private final DoctorRepository doctorRepository;
+    private final PatientRepository patientRepository;
+    private final TbUserRepository tbUserRepository;
+
+    public ConsultationMapping(DoctorRepository doctorRepository,
+                               PatientRepository patientRepository,
+                               TbUserRepository tbUserRepository) {
+        this.doctorRepository = doctorRepository;
+        this.patientRepository = patientRepository;
+        this.tbUserRepository = tbUserRepository;
+    }
+
+
+    public Consultation toEntity(ConsultationDto consultationDto) {
+        Doctor doctor = doctorRepository.findById(consultationDto.doctorId())
+                .orElseThrow(() -> new RuntimeException("Doctor not found" + consultationDto.doctorId()));
+        Patient patient = patientRepository.findById(consultationDto.patientId())
+                .orElseThrow(() -> new RuntimeException("Patient not found" + consultationDto.patientId()));
+        TbUser tbUser = tbUserRepository.findById(consultationDto.createdBy())
+                .orElseThrow(() -> new RuntimeException("TbUser not found" + consultationDto.createdBy()));
+
+        Consultation consultation = new Consultation();
+        consultation.setDoctor(doctor);
+        consultation.setConsultationDate(consultationDto.consultationDate());
+        consultation.setPatient(patient);
+        consultation.setConsultationStatus(consultationDto.consultationStatus());
+        consultation.setReason(consultationDto.reason());
+        consultation.setNote(consultationDto.note());
+        consultation.setCreatedBy(tbUser);
+
+
+        return consultation;
+    }
+    public ConsultationDto toDto(Consultation consultation) {
+        return new ConsultationDto(
+                consultation.getId(),
+                consultation.getDoctor().getId(),
+                consultation.getPatient().getId(),
+                consultation.getConsultationDate(),
+                consultation.getConsultationStatus(),
+                consultation.getReason(),
+                consultation.getNote(),
+                consultation.getCreatedBy().getId()
+        );
+    }
 }
--- src/main/java/dev/clinican/service/ConsultationService.java
+++ src/main/java/dev/clinican/service/ConsultationService.java
@@ -6,6 +6,7 @@ import dev.clinican.entity.Doctor;
 import dev.clinican.entity.Patient;
 import dev.clinican.entity.TbUser;
 import dev.clinican.entity.enums.ConsultationStatus;
+import dev.clinican.mapping.ConsultationMapping;
 import dev.clinican.repository.ConsultationRepository;
 import dev.clinican.repository.DoctorRepository;
 import dev.clinican.repository.PatientRepository;
@@ -25,56 +26,20 @@ public class ConsultationService {
     private final PatientRepository patientRepository;
     private final ConsultationHistoryService consultationHistoryService;
     private final TbUserRepository tbUserRepository;
+    private final ConsultationMapping consultationMapping;
 
-    // Injection
     public ConsultationService(ConsultationRepository consultationRepository,
                                DoctorRepository doctorRepository,
                                PatientRepository patientRepository,
                                ConsultationHistoryService consultationHistoryService,
-                               TbUserRepository tbUserRepository) {
+                               TbUserRepository tbUserRepository,
+                               ConsultationMapping consultationMapping) {
         this.consultationRepository = consultationRepository;
         this.doctorRepository = doctorRepository;
         this.patientRepository = patientRepository;
         this.consultationHistoryService = consultationHistoryService;
         this.tbUserRepository = tbUserRepository;
-    }
-
-
-
-
-    //  DTO to Entity (Entry)
-    private Consultation toEntity(ConsultationDto consultationDto) {
-        Doctor doctor = doctorRepository.findById(consultationDto.doctorId())
-                .orElseThrow(() -> new RuntimeException("Doctor not found" + consultationDto.doctorId()));
-        Patient patient = patientRepository.findById(consultationDto.patientId())
-                .orElseThrow(() -> new RuntimeException("Patient not found" + consultationDto.patientId()));
-        TbUser tbUser = tbUserRepository.findById(consultationDto.createdBy())
-                .orElseThrow(() -> new RuntimeException("TbUser not found" + consultationDto.createdBy()));
-
-        Consultation consultation = new Consultation();
-        consultation.setDoctor(doctor);
-        consultation.setConsultationDate(consultationDto.consultationDate());
-        consultation.setPatient(patient);
-        consultation.setConsultationStatus(consultationDto.consultationStatus());
-        consultation.setReason(consultationDto.reason());
-        consultation.setNote(consultationDto.note());
-        consultation.setCreatedBy(tbUser);
-
-
-        return consultation;
-    }
-    // Entity to DTO (Exit)
-    private ConsultationDto toDto(Consultation consultation) {
-        return new ConsultationDto(
-                consultation.getId(),
-                consultation.getDoctor().getId(),
-                consultation.getPatient().getId(),
-                consultation.getConsultationDate(),
-                consultation.getConsultationStatus(),
-                consultation.getReason(),
-                consultation.getNote(),
-                consultation.getCreatedBy().getId()
-        );
+        this.consultationMapping = consultationMapping;
     }
 
     // Public Methods
@@ -82,7 +47,7 @@ public class ConsultationService {
     // Create
     public ConsultationDto create(ConsultationDto consultationDto) {
 
-        Consultation consultation = toEntity(consultationDto);
+        Consultation consultation = consultationMapping.toEntity(consultationDto);
         Consultation savedConsultation = consultationRepository.save(consultation);
 
         consultationHistoryService.recordChange(
@@ -92,7 +57,7 @@ public class ConsultationService {
                 savedConsultation.getCreatedBy()
         );
 
-        return toDto(savedConsultation);
+        return consultationMapping.toDto(savedConsultation);
 
     }
 
@@ -127,7 +92,7 @@ public class ConsultationService {
                 savedConsultation.getConsultationStatus(),
                 savedConsultation.getCreatedBy()
         );
-        return toDto(savedConsultation);
+        return consultationMapping.toDto(savedConsultation);
     }
 
     //  Delete
@@ -138,7 +103,7 @@ public class ConsultationService {
     // List By ID
     public ConsultationDto findById(UUID id) {
         return consultationRepository.findById(id)
-                .map(this::toDto)
+                .map(consultationMapping::toDto)
                 .orElseThrow(() -> new RuntimeException("Consultation not found" + id));
     }
 
@@ -146,7 +111,7 @@ public class ConsultationService {
     public List<ConsultationDto> findByObservation(String observation) {
         return consultationRepository.findByNoteContainingIgnoreCase(observation)
                 .stream() // Take the list one by one
-                .map(this::toDto)// Convert in Dto
+                .map(consultationMapping::toDto)// Convert in Dto
                 .toList(); // List
     }
 
@@ -155,7 +120,7 @@ public class ConsultationService {
         return consultationRepository
                 .findByDoctorUserNameContainingIgnoreCase(name)
                 .stream() // Take the list one by one
-                .map(this::toDto)// Convert in Dto
+                .map(consultationMapping::toDto)// Convert in Dto
                 .toList(); // List
     }
 
@@ -164,14 +129,14 @@ public class ConsultationService {
         return consultationRepository
                 .findByPatientUserNameContainingIgnoreCase(name)
                 .stream()
-                .map(this::toDto)
+                .map(consultationMapping::toDto)
                 .toList();
     }
 
     public List<ConsultationDto> findAll() {
         return consultationRepository.findAll()
                 .stream()
-                .map(this::toDto)
+                .map(consultationMapping::toDto)
                 .toList();
     }
 
